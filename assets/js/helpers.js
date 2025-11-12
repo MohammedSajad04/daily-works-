19e4833e47556480dccd57b4141348d98a35ce49
mohammed sajad <sajadsumanclt@gmail.com>
2025-11-12 11:04:00 +0530

    refactor: improve date formatter

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -152,3 +152,5 @@ const formatLongDate = (date) => new Date(date).toLocaleDateString('en-US', { we
 /* Updated: 202511102042 */
 const formatDate = (date) => new Date(date).toLocaleDateString();
 /* Updated: 202511111617 */
+const formatLongDate = (date) => new Date(date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+/* Updated: 202511121104 */
