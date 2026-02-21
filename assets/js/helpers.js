8ce0565d9e316fe210e5d701ba7b8d984288fdc0
mohammed sajad <sajadsumanclt@gmail.com>
2026-02-21 10:55:00 +0530

    feat: add date formatter utility

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -314,3 +314,5 @@ const formatLongDate = (date) => new Date(date).toLocaleDateString('en-US', { we
 /* Updated: 202602181854 */
 const formatLongDate = (date) => new Date(date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 /* Updated: 202602201911 */
+const formatDate = (date) => new Date(date).toLocaleDateString();
+/* Updated: 202602211055 */
